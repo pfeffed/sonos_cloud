1c72080bf8c99c5109a7a38d6457c5d533d5925c
David Pfeffer <david@pfeffer.org>
2014-01-30 11:24:19 -0800

    added ability to use friendly speaker name and improved error responses

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 gem 'thin'
 gem 'sinatra'
-gem 'sonos', :git => 'git://github.com/pfeffed/sonos.git', branch: 'master'
+gem 'sonos', :git => 'git://github.com/pfeffed/sonos.git', :branch => 'master'
--- home_remote_server.rb
+++ home_remote_server.rb
@@ -16,33 +16,52 @@ class HomeRemoteServer < Sinatra::Base
 		sendCommand(params[:speaker], params[:command])
 	end
 
-	def sendCommand(speaker, command)
+	def sendCommand(speaker_id, command)
 		begin
 			system = Sonos::System.new
-			speaker = system.find_speaker_by_uid(speaker)
+			speaker = system.find_speaker_by_uid(speaker_id)
+			speaker = system.find_speaker_by_name(speaker_id) if speaker.nil?
+			return send_error_response "Speaker '#{speaker_id}' could not be found." if speaker.nil?
 			case command
 			when 'play'
 			  speaker.play
+			  return 200
 			when 'pause'
 				speaker.pause
+			  return 200
 			when 'stop'
 			  speaker.stop
+			  return 200
 			when 'next'
 			  speaker.stop
+			  return 200
 			when 'prev'
 			  speaker.stop
+			  return 200
 			when 'volup'
 				speaker.unmute if speaker.muted?
 			  speaker.volume = speaker.volume + 5
+			  return 200
 			when 'voldown'
 				speaker.unmute if speaker.muted?
 			  speaker.volume = speaker.volume - 5
+			  return 200
 			when 'mute'
 			  speaker.muted? ? speaker.unmute : speaker.mute
+			  return 200
+			else
+				return send_error_response "The command '#{command}' is not currently supported by this remote."
 			end
 		rescue
-			speaker = system.find_speaker_by_uid(speaker)
-			"#{speaker.name} had an error when trying to process the #{command} command." 
+			begin
+				return send_error_response "An unknown error occurred."
+			rescue
+			end
 		end
+	end
+
+	def send_error_response(error_msg)
+		puts error_msg
+		[500, error_msg]
 	end
 end
